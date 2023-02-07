e2b31d6dcda927bc6f07c214bdd7d3cbf966be88
Frasan00 <francesco.sangiovanni.7@gmail.com>
2023-02-07 18:22:37 +0100

    doesnt work

---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+docker compose build
+# remeber to add the path of your folder in the shared files in dockers options
+docker compose up -d
---
+++ server/src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/api', (req, res) => {
+    res.send("Nodejs Api");
+})
+
+
+app.listen(PORT, () => console.log("Listening on port "+PORT));
