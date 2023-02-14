9a06379214790c38c0a87aed50ca6c2809279de2
Frasan00 <francesco.sangiovanni.7@gmail.com>
2023-02-14 20:45:36 +0100

    works till logged

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 ### App with keylockr and nginx as a reverse proxy ###
 
 - This is a simple authentication app that focuses on keycloack and nginx as a reverse proxy integration
-- The simplest way to run this application is via docker-compose with "run.sh"
+- The simplest way to run this application is via docker-compose with "bash run.sh"
+
+### keycloak setup ###
+
+- In order to setup keycloak you have to those actions in keycloak dashboard on "localhost/auth" or "localhost:2000/auth":
+
+1) Create a realm (remember to change env var properly to match your realm name, default: react-realm)
+2) Create a client (remember to change env var properly to match your client name, default: user)
+3) Set up client valid_redirect_uri to "localhost/client/"
+4) Set up the kind of authentication you want (register, email, remember me etc.)
--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import axios from "axios";
+import Keycloak from "keycloak-js";
 import './App.css';
-import { connect } from "./keycloack/connection";
 import React, {useEffect, useState} from "react";
 
 
@@ -16,7 +16,23 @@ function App() {
     if (!process.env.REACT_APP_CLIENTID){ console.log("REACT_APP_CLIENTID env variable not set in .env"); }
 
     // keycloak config
-    connect()
+    const keycloak = new Keycloak({
+      url: process.env.REACT_APP_URL,
+      realm: process.env.REACT_APP_REALM,
+      clientId: process.env.REACT_APP_CLIENTID,
+    });
+
+    keycloak.init({ onLoad: 'login-required', redirectUri: 'http://localhost:/logged' }).then((res) => {
+        if (res) {
+        console.log('User is authenticated');
+        console.log("token: "+keycloak.token);
+        setJwt(keycloak.token);
+        setIsLogged(true);
+        return;
+    } else {
+        console.log('User is not authenticated');
+        }
+    });
   }, [])
   
   return (
