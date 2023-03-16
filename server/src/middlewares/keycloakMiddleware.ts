af01973069adefe3cc7d9360efe5653f736adab2
Frasan00 <francesco.sangiovanni.7@gmail.com>
2023-03-16 15:37:04 +0100

    jwt authentication resolved

--- server/src/middlewares/keycloakMiddleware.ts
+++ server/src/middlewares/keycloakMiddleware.ts
@@ -1,6 +1,6 @@
 
 import { Request, Response, NextFunction } from "express";
-import request from "request";
+import axios from "axios";
 
 
 // middleware to validate tokens from keycloak
@@ -11,26 +11,17 @@ export const keycloakMiddleware = async (req: Request, res: Response, next: Next
     const payload = JSON.parse(Buffer.from(parts[1], 'base64').toString());
     const issuer = payload.iss;
  
-    const config = {
-        method: 'GET',
-        url: `${issuer}/protocol/openid-connect/userinfo`,
+    try {
+      console.log(`Sending request to: ${issuer}/protocol/openid-connect/userinfo`);
+      const response = await axios.get(`${issuer}/protocol/openid-connect/userinfo`, {
         headers: {
           Authorization: `Bearer ${token}`,
         },
-    }
-
-    request(config, (error: any, response: any, body: any) => {
-        console.log(`Request sent to: ${issuer}/protocol/openid-connect/userinfo`);
-        console.log(config);
-        if (error) { 
-          return res.status(400).json(error);
-        }
-  
-        // invalid token
-        if (response.statusCode !== 200) {
-          console.log("Not authorized transaction");
-          return res.status(401).json(body);
-        }
-        next();
       });
+      console.log(JSON.stringify(response.data));
+      next();
+    } catch (error: any) {
+      console.error(error);
+      return res.status(401).json(error);
+    }
 }
